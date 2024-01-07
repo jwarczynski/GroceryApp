9f50f29f1fb293016686caf08ab24e86364c1156
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2024-01-07 22:03:08 +0100

    enable to add and edit groceries

---
+++ GroceryApp.WPF/Pages/Groceries/EditGroceryPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Warczynski.Zbaszyniak.GroceryApp.BLC;
+using Warczynski.Zbaszyniak.GroceryApp.Interfaces;
+
+namespace GroceryApp.WPF.Pages.Groceries;
+
+public partial class EditGroceryPage : Page
+{
+    private IGrocery Grocery { get; }
+    public EditGroceryPage(IGrocery grocery)
+    {
+        InitializeComponent();
+        Grocery = grocery;
+        DataContext = Grocery;
+    }
+
+    private void Save_Click(object sender, RoutedEventArgs e)
+    {
+        var saved = Grocery.Id == null ? BLCContainer.Instance.SaveGrocery(Grocery) : BLCContainer.Instance.EditGrocery(Grocery);
+        ShowSuccessMessage(saved);
+        NavigationService?.GoBack();
+    }
+
+    private void ShowSuccessMessage(IGrocery saved)
+    {
+        MessageBox.Show($"Saved Grocery: " + "\n" +
+                        $" Id:  {saved.Id}" + "\n" +
+                        $" Name: {saved.Name}" + "\n" +
+                        $" Address: {saved.Address}");
+    }
+}
