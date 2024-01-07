4c2ec1a06a76f0c71fbe45e4a04e113530e0b801
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2024-01-07 22:02:43 +0100

    move files

---
+++ GroceryApp.WPF/Pages/Groceries/GroceriesPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using GroceryApp.WPF.Pages.Groceries;
+using Warczynski.Zbaszyniak.GroceryApp.BLC;
+using Warczynski.Zbaszyniak.GroceryApp.Interfaces;
+
+namespace GroceryApp.WPF;
+
+public partial class GroceriesPage : Page
+{
+    private readonly BLC _blc = BLCContainer.Instance;
+    private readonly ObservableCollection<IGrocery> _groceries;
+    
+    public GroceriesPage()
+    {
+        InitializeComponent();
+        _groceries = new ObservableCollection<IGrocery>(_blc.GetAllGroceries());
+        GroceriesDataGrid.ItemsSource = _groceries;
+    }
+
+    private void Remove_Click(object sender, RoutedEventArgs e)
+    {
+        var button = (Button)sender;
+        var grocery = (IGrocery)button.DataContext;
+        _blc.DeleteGrocery(grocery);
+        _groceries.Remove(grocery);;
+    }
+
+    private void Update_Click(object sender, RoutedEventArgs e)
+    {
+        var button = (Button)sender;
+        var grocery = (IGrocery)button.DataContext;
+        var detailPage = new EditGroceryPage(grocery);
+        
+        NavigationService?.Navigate(detailPage);;
+    }
+
+    private void Add_Click(object sender, RoutedEventArgs e)
+    {
+        var detailPage = new EditGroceryPage(_blc.GetGroceryTemplate());
+        NavigationService?.Navigate(detailPage);
+    }
+}
