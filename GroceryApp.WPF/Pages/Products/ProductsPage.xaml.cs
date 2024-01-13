23a66e153cb76bd53250e49aad04eee43f1db9bb
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2024-01-13 13:18:51 +0100

    apply MVVM pattern to products pages

--- GroceryApp.WPF/Pages/Products/ProductsPage.xaml.cs
+++ GroceryApp.WPF/Pages/Products/ProductsPage.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows;
-using System.Windows.Controls;
-using GroceryApp.WPF.ExtensionClass;
-using GroceryApp.WPF.Filters;
+﻿using System.Windows.Controls;
+using GroceryApp.WPF.ViewModels;
 using Warczynski.Zbaszyniak.GroceryApp.BLC;
 using Warczynski.Zbaszyniak.GroceryApp.Core;
 using Warczynski.Zbaszyniak.GroceryApp.Interfaces;
@@ -12,88 +9,28 @@ namespace GroceryApp.WPF.Pages.Products;
 public partial class ProductsPage
 {
     private readonly BLC _blc = BLCContainer.Instance;
-    private readonly ObservableCollection<IProduct> _products;
 
     public static ProductCategory[] ProductCategoryEnumValues =>
         (ProductCategory[])Enum.GetValues(typeof(ProductCategory));
 
-    public ProductCategory SelectedCategory { get; set; }
-    private List<ProductCategory> SelectedCategories { get; set; } = new List<ProductCategory>();
-    
-    public string? NameFilter { get; set; }
-    public float MinPrice { get; set;}
-    public float MaxPrice { get; set; }
-
     public ProductsPage()
     {
         InitializeComponent();
-        _products = new ObservableCollection<IProduct>(_blc.GetAllProducts());
-        ProductsDataGrid.ItemsSource = _products;
-        IsVisibleChanged += ProductsPage_IsVisibleChanged;
-    }
-
-    private void Add_Click(object sender, RoutedEventArgs e)
-    {
-        var detailPage = new EditProductPage(_blc.GetProductTemplate());
-        NavigationService?.Navigate(detailPage);
-        
-        // AddProductControl.Visibility = Visibility.Visible;
-    }
-
-    private void Remove_Click(object sender, RoutedEventArgs e)
-    {
-        var button = (Button)sender;
-        var product = (IProduct)button.DataContext;
-        _blc.DeleteProduct(product);
-        _products.Remove(product);
-    }
-
-    private void Update_Click(object sender, RoutedEventArgs e)
-    {
-        var button = (Button)sender;
-        var product = (IProduct)button.DataContext;
-        var detailPage = new EditProductPage(product);
-        
-        NavigationService.Navigate(detailPage);
-    }
-
-    private void ApplyFilters_Click(object sender, RoutedEventArgs e)
-    {
-        _products.Clear();
-        _products.AddRange(_blc.GetProductsByFilter(CreateFilter()));
-    }
-
-    private Filter CreateFilter()
-    {
-        Filter myFilter = new Filter
-        {
-            // Categories = new List<ProductCategory> { SelectedCategory },
-            Categories = SelectedCategories,
-            Name = NameFilter,
-            MinPrice = MinPrice,
-            MaxPrice = MaxPrice
-        };
-        return myFilter;
-    }
-
-    private void AddProductControl_ProductSaved(object? sender, ProductEventArgs e)
-    {
-        var saved = _blc.SaveProduct(e.Product);
-        _products.Add(saved);
-        MessageBox.Show($"Product saved: {saved.Name}");
+        var viewModel = (ProductsViewModel)DataContext;
+        viewModel.RequestNavigation += NavigateToProductDetail;
     }
     
-    private void ProductsPage_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+    private void NavigateToProductDetail(IProduct product)
     {
-        if ((bool)e.NewValue)
-        {
-            _products.Clear();
-            _products.AddRange(_blc.GetAllProducts());
-        }
+        var viewModel = (ProductsViewModel)DataContext;
+        var detailPage = new EditProductPage(product, viewModel.Products);
+        NavigationService?.Navigate(detailPage);
     }
-
+    
+    // Property SelectedItems is readonly, so we need to handle the event
     private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        SelectedCategories = ((ListBox)sender).SelectedItems.Cast<ProductCategory>().ToList();;
+        var model = (ProductsViewModel)DataContext;
+        model.Filter.Categories = ((ListBox)sender).SelectedItems.Cast<ProductCategory>().ToList();
     }
 }
