52d6b89c8c33a593dafda553b62df4b726bcdfff
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2024-01-19 20:58:02 +0100

    add sqlite database with migration

---
+++ GroceryApp.SQLDao/Migrations/20240102223015_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Warczynski.Zbaszyniak.GroceryApp.DAOMock1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groceries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groceries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<float>(type: "REAL", nullable: false),
+                    Category = table.Column<int>(type: "INTEGER", nullable: false),
+                    Magnesium = table.Column<float>(type: "REAL", nullable: false),
+                    Potassium = table.Column<float>(type: "REAL", nullable: false),
+                    Sodium = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Groceries");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ GroceryApp.SQLDao/Migrations/20240102224903_[MigrationName].cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Warczynski.Zbaszyniak.GroceryApp.DAOMock1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GroceryEntityId",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_GroceryEntityId",
+                table: "Products",
+                column: "GroceryEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Groceries_GroceryEntityId",
+                table: "Products",
+                column: "GroceryEntityId",
+                principalTable: "Groceries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Groceries_GroceryEntityId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_GroceryEntityId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "GroceryEntityId",
+                table: "Products");
+        }
+    }
+}
