357472d87d9593e5f73781aeb90b0f75012b0e4d
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2024-01-29 14:14:47 +0100

    script for copying SQLDAO.dll to projects

--- GroceryApp.WPF/CopyFiles.ps1
+++ GroceryApp.WPF/CopyFiles.ps1
@@ -1,7 +1,9 @@
 ﻿$sourcePath = "GroceryApp.SQLDao\bin\Debug\net8.0"
 $destinationPath = "GroceryApp.WPF\bin\Debug\net8.0-windows"
+$destinationPath2 = "GroceryApp.MVC"
 
 Write-Host "Copying GroceryApp.SQLDao.dll..."
 Copy-Item -Path "$sourcePath\GroceryApp.SQLDao.dll" -Destination $destinationPath -Force
+Copy-Item -Path "$sourcePath\GroceryApp.SQLDao.dll" -Destination $destinationPath2 -Force
 
 Write-Host "Copy complete."
